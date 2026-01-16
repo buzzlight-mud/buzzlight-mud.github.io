3fb8e36db4e6f9ed3a28c11a9629957c2b0d379b
buzzlight <319290990@qq.com>
2026-01-16 17:07:09 +0800

    Site updated: 2026-01-16 17:07:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/01/16/周至/","2026/01/14/hello-world/","2026/01/15/周记-1-15/"];function toRandomPost(){
+var posts=["2026/01/14/hello-world/","2026/01/15/周记-1-15/","2026/01/16/周至/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
